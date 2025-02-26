9e80dff0f7222078709812fcd1a4a345fee1fdbb
pengh618 <pengh618@gmail.com>
2025-02-26 20:07:17 +0800

    1

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from public.usage import USAGE as html
 from api.hello import router as hello_router
 from fastapi import FastAPI
 from fastapi.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
 app.include_router(hello_router, prefix="/ai")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/")
 def _root():
