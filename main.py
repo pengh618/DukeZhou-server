db1072f17c2faec9025e8b8c3ceaef8617742a90
pengh618 <pengh618@gmail.com>
2025-03-02 19:16:17 +0800

    1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from api.qa_plus import qa_plus
+from api.qa_plus import router as qa_plus_router
 from api.hello import router as hello_router
 
 from public.usage import USAGE as html
@@ -10,7 +10,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 app.include_router(hello_router, prefix="/ai")
-app.include_router(qa_plus.router, prefix="/qa_plus")
+app.include_router(qa_plus_router, prefix="/qa_plus")
 
 app.add_middleware(
     CORSMiddleware,
